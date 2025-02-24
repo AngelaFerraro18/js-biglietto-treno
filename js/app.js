0332a60d309983d9180048bde7b8622681404801
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-02-24 19:16:07 +0100

    feat: optimizing app code

--- js/app.js
+++ js/app.js
@@ -14,22 +14,17 @@ let ticketPrice = kilometres * rateKm;
 
 if (age < 18) {
     //se l'utente è minorenne, allora avrà uno sconto del 20%
-    const rateKm = 0.21;
-    let ticketPrice = kilometres * rateKm;
     const discount = 20;
     ticketPrice = ticketPrice - ((ticketPrice * discount) / 100);
     //il prezzo finale sarà stampato con massimo due decimali
-    console.log(`Il costo del tuo biglietto è ${ticketPrice.toFixed(2)}€`);
+
 } else if (age >= 65) {
     //se l'utente è over 65, allora avrà uno sconto del 40%
-    const rateKm = 0.21;
-    let ticketPrice = kilometres * rateKm;
     const discount = 40;
     ticketPrice = ticketPrice - ((ticketPrice * discount) / 100);
     //il prezzo finale sarà stampato con massimo due decimali
-    console.log(`Il costo del tuo biglietto è ${ticketPrice.toFixed(2)}€`);
-} else {
-    //se nessuna delle condizioni precedenti è vera, l'utente pagherà il prezzo pieno
-    //il prezzo finale sarà stampato con massimo due decimali
-    console.log(`Il costo del tuo biglietto è ${ticketPrice.toFixed(2)}€`);
+
 }
+//se nessuna delle condizioni precedenti è vera, l'utente pagherà il prezzo pieno
+//il prezzo finale sarà stampato con massimo due decimali
+console.log(`Il costo del tuo biglietto è ${ticketPrice.toFixed(2)}€`);
