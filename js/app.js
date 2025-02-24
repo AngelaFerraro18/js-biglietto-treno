6d3e49db9cf7af15e0c01ee49aeb713097add7e4
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-02-24 15:18:51 +0100

    feat: improving ticket section of the app

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-const kilometres = parseInt(prompt('Ciao! Quanti chilometri devi percorrere?'));
+const kilometres = parseInt(prompt('Ciao! Quanti chilometri vuoi percorrere?'));
 console.log(kilometres);
 const age = parseInt(prompt('Scrivi la tua età:'));
 console.log(age);
 
 const rateKm = 0.21;
-let ticketFullPrice = kilometres * rateKm;
-console.log(ticketFullPrice);
 
-if (age < 18){
-    discountTeen = 20; 
+
+if (age < 18) {
+    discountTeen = 20;
     teenTicket = (ticketFullPrice * discountTeen) / 100;
     console.log(`Il costo del tuo biglietto è ${teenTicket}€`);
-} else if (age >= 65){
-    discountSenior = 40; 
+} else if (age >= 65) {
+    discountSenior = 40;
     seniorTicket = (ticketFullPrice * discountSenior) / 100;
     console.log(`Il costo del tuo biglietto è ${seniorTicket}€`);
+} else {
+    ticketFullPrice = kilometres * rateKm;
+    console.log(`Il costo del tuo biglietto è ${ticketFullPrice}€`);
 }
