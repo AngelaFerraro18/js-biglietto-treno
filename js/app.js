2e4bd2d21140b94168a5b12410030e88b123b017
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-02-24 15:13:52 +0100

    feat: add discount tickets

--- js/app.js
+++ js/app.js
@@ -4,5 +4,15 @@ const age = parseInt(prompt('Scrivi la tua età:'));
 console.log(age);
 
 const rateKm = 0.21;
-const ticketFullPrice = kilometres * rateKm;
+let ticketFullPrice = kilometres * rateKm;
 console.log(ticketFullPrice);
+
+if (age < 18){
+    discountTeen = 20; 
+    teenTicket = (ticketFullPrice * discountTeen) / 100;
+    console.log(`Il costo del tuo biglietto è ${teenTicket}€`);
+} else if (age >= 65){
+    discountSenior = 40; 
+    seniorTicket = (ticketFullPrice * discountSenior) / 100;
+    console.log(`Il costo del tuo biglietto è ${seniorTicket}€`);
+}
